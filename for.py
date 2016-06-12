942f2fb792966a2c580e735de51dabc68ed28f59
Chris <zc850926@163.com>
2016-06-12 16:06:09 +0800

    add for

---
+++ for.py
@@ -0,0 +1,14 @@
+for x in "abcd":
+    print x , "hello world"
+for x in [0,1,2,3,4,5]:
+    print x+1 , x
+print "++++++++++++++++++++++++++++++"
+for x in range(1,11,1):
+    print x
+
+print "++++++++++++++++++++++++++++++"
+y=0    
+for x in range(1,101):
+
+    y=y+x
+print y
