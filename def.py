fa66f7fd4df69d5a9fc8d66cfc6dde528b908bf4
Chris <zc850926@163.com>
2016-06-13 17:01:48 +0800

    update args kwargs

--- def.py
+++ def.py
@@ -1,6 +1,19 @@
 
-def fun():
-    if True:
-        print "good"
+# def fun():
+#     if True:
+#         print "good"
         
-fun()
+# fun()
+
+
+def fun(*args):
+    print args
+    for i in args :
+        print i
+    
+fun(1,2,3)
+
+def fun1(**kwargs):
+    print kwargs
+
+fun1(x=1,y=2)
