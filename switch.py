c0b47460ac9da20a87795bdf506f0824322e50a8
Chris <zc850926@163.com>
2016-06-14 10:30:58 +0800

    add like switch solution

---
+++ switch.py
@@ -0,0 +1,24 @@
+
+from __future__ import division
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+    
+def mul(x,y):
+    return x*y
+    
+def div(x,y):
+    return x/y
+    
+operator={"+":add,"-":sub,"*":mul,"/":div}
+
+# print operator["+"]
+# print add
+# print operator["+"](3,4)
+
+def oper(x,o,y):
+    print operator.get(o)(x,y)
+    
+oper(3,"/",2)
